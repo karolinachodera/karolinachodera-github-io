752cafdeee82a83218912681d1e5438842709221
Karolina Chodera <karolinachodera@gmail.com>
2017-05-06 14:06:20 +0200

    TODO add new item

--- todo/js/app.js
+++ todo/js/app.js
@@ -27,9 +27,9 @@ app.service("todo_list", function() {
 	var todoList = {};
 	
 	todoList.items = [
-		{complated: true, name: "Shopping", date: "2017-05-05"},
-		{complated: true, name: "Programming", date: "2017-05-03"},
-		{complated: true, name: "Eating", date: "2017-05-04"}
+		{id: 1, complated: true, name: "Shopping", date: "2017-05-05"},
+		{id: 2,complated: true, name: "Programming", date: "2017-05-03"},
+		{id: 3,complated: true, name: "Eating", date: "2017-05-04"}
 	]
 	
 	return todoList;
@@ -41,6 +41,7 @@ app.controller("startList", function($scope) {
 	$scope.list.title = "";
 });
 
-app.controller("todoItemsCtrl", function($scope, todo_list) {
+app.controller("todoItemsCtrl", function($scope, todo_list, $location) {
 	$scope.listItems = todo_list.items;
+	$scope.listItem = {id: 4, completed: true, name: "zadanie", date: new Date()}
 });
