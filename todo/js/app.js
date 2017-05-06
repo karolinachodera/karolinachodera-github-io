e34c13d21e80bb27c04ef56ca176db42df108ee5
Karolina Chodera <karolinachodera@gmail.com>
2017-05-06 18:58:00 +0200

    TODO add item

--- todo/js/app.js
+++ todo/js/app.js
@@ -4,7 +4,6 @@ app.config(function($routeProvider) {
 	$routeProvider
 		.when("/", {
 			templateUrl: "startList.html",
-			controller: "startList"
 		})
 		
 		.when("/list", {
@@ -27,9 +26,6 @@ app.service("todo_list", function() {
 	var todoList = {};
 	
 	todoList.items = [
-		{id: 1, complated: true, name: "Shopping", date: "2017-05-05"},
-		{id: 2,complated: true, name: "Programming", date: "2017-05-03"},
-		{id: 3,complated: true, name: "Eating", date: "2017-05-04"}
 	]
 	
 	todoList.save = function(newItem) {
@@ -47,7 +43,7 @@ app.controller("startList", function($scope) {
 
 app.controller("todoItemsCtrl", function($scope, todo_list, $location) {
 	$scope.listItems = todo_list.items;
-	$scope.listItem = {id: 4, completed: true, name: "zadanie", date: new Date()}
+	$scope.listItem = {id: 1, completed: true, name: "zadanie", date: new Date()}
 	$scope.save = function() {
 		todo_list.save($scope.listItem);
 		$location.path("/list");
