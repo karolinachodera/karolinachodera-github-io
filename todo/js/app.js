829efc936e84a1e40f93d4e69fceaf45dfc7e16f
Karolina Chodera <karolinachodera@gmail.com>
2017-05-07 14:29:25 +0200

    TODO item completed

--- todo/js/app.js
+++ todo/js/app.js
@@ -39,12 +39,6 @@ app.service("todo_list", function() {
 	
 	todoList.complete = function(entry) {
 		entry.completed = !entry.completed;
-		if(entry.completed) {
-			var  index = todoList.items.indexOf(entry);
-			todoList.items.push(todoList.items[index]);
-			todoList.items.splice(index, 1);
-			return todoList;
-		}
 	};
 	
 	return todoList;
