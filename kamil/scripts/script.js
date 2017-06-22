37ee26ac0c3e393c152d7725c626c764da9997d1
Karolina Chodera <karolinachodera@gmail.com>
2017-06-22 14:35:43 +0200

    Kamil - RWD

--- kamil/scripts/script.js
+++ kamil/scripts/script.js
@@ -1,5 +1,5 @@
 function initMap() {
-	var uluru = {lat: 53.0302626, lng: 18.654473};
+	var uluru = {lat: 53.030696, lng: 18.6573606};
 	var map = new google.maps.Map(document.getElementById('map'), {
 	  zoom: 12,
 	  center: uluru
