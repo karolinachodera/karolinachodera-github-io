b3e0ce8b3f6ca6a308db982e3df201ef76388d4f
Karolina Chodera <karolinachodera@gmail.com>
2017-06-23 12:15:58 +0200

    Kamil - add button, changing color of nav during scrolling

--- kamil/scripts/script.js
+++ kamil/scripts/script.js
@@ -9,3 +9,41 @@ function initMap() {
 	  map: map
 	});
 }
+
+(function buttonUp() {
+	var button = document.querySelector(".up");
+	
+	function buttonVisibility() {
+		if(window.scrollY >= 100 || document.documentElement.scrollTop >= 100) {
+			button.classList.remove("hidden");
+		} else {
+			button.classList.add("hidden")
+		}
+	}
+	
+	function scrollUp() {
+		if(window.scrollY >= 100 || document.documentElement.scrollTop >= 100) {
+			window.scrollBy(0, -30);
+			window.setTimeout(scrollUp, 5);
+		}
+	}
+	
+	window.addEventListener("scroll", buttonVisibility, false);
+	button.addEventListener("click", scrollUp, false);
+})();
+
+(function navFullColor() {
+	var nav = document.querySelector("nav");
+	
+	function fullColor() {
+		if(window.scrollY >= 300 || document.documentElement.scrollTop >= 300) {
+			nav.classList.add("fullcolor");
+		} else {
+			nav.classList.remove("fullcolor");
+		}
+	}
+	
+	window.addEventListener("scroll", fullColor, false);
+})();
+
+
